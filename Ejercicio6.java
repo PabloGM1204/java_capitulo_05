e4585be1debbdbab6cec0bdad4b836e3798f296e
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-18 21:20:16 +0200

    Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,11 @@
+public class Ejercicio6{
+    public static void main(String [] args) {
+        System.out.println("Este programa muestra los números del 320 al 160, contando de 20 en 20 hacia atrás");
+        int i = 340;
+        do {
+            System.out.println(i-=20);
+        } while(i>=180);
+
+        
+    }
+}
