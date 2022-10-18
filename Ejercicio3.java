a35aede565523825ceb0e90557232b766b6b4cbe
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-18 21:09:44 +0200

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3{
+    public static void main(String [] args) {
+        System.out.println("Este progama te dice los multiplos de 5");
+        int i =0;
+        do{
+            System.out.println(i*5);
+            i++;
+        } while(i<=20);
+    }
+}
