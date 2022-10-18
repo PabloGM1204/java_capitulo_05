c0bcbf48e004abbf7ee555eafe233c6425c0054e
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-18 20:51:54 +0200

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1{
+    public static void main(String [] args) {
+        System.out.println("Este programa te dice los multiplos de 5 de 0 a 100");
+        int n = 5;
+        for(int i = 0; i<=20; i++){
+            System.out.println(n*i);
+        }   
+    }
+}
