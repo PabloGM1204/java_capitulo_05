dc77772342a873099a42608b372f8d8cc54e7b3f
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-18 20:56:24 +0200

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2{
+    public static void main(String [] args){
+        System.out.println("Este programa te dice los múltiplos de 5");
+        int i =0;
+        while(i<=20){
+            System.out.println(5*i);
+            i++;
+        }
+    }
+}
