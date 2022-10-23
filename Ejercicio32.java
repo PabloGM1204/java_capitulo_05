68fdcfb2ab82b8591fa584adc019fc1457d14660
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-23 18:13:16 +0200

    Ejercicio32

--- Ejercicio32.java
+++ Ejercicio32.java
@@ -5,8 +5,8 @@ public class Ejercicio32{
         System.out.println("Este programa te coge los números pares de un número y los suma");
         System.out.print("Dime un número entero: ");
         long n = sc.nextInt();
-        int c = 0;
-        int t = 0;
+        long c = 0;
+        long t = 0;
         while(n>0){
             c = (int)n%10;
             if((c%2)==0){
@@ -16,5 +16,6 @@ public class Ejercicio32{
             n /=10;
         }
         System.out.println("La suma total es: "+t);
+        sc.close();
     }
 }
