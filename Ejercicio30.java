e4bcd77716d9e587f04db26d5718a9aee8ca7ed0
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-23 18:03:41 +0200

    Ejercicio30

---
+++ Ejercicio30.java
@@ -0,0 +1,5 @@
+public class Ejercicio30{
+    public static void main(String [] args) {
+        System.out.println("Este programa calcula las horas entre dos dias de una semana posterior");
+    }
+}
