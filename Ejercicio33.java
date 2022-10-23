8b53aec77d46b6a0846be5921f5ee15b49315871
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-23 18:20:13 +0200

    Ejercicio33

--- Ejercicio33.java
+++ Ejercicio33.java
@@ -3,7 +3,19 @@ public class Ejercicio33{
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Este programa pinta una U");
-        System.out.print("Dime la altura de la U");
+        System.out.print("Dime la altura de la U: ");
         int alt = sc.nextInt();
+        for(int i = 0;i<alt-1;i++ ){
+            System.out.print("*");
+            for(int e = 0; e<alt-2; e++){
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        System.out.print(" ");
+        for(int e = 0;e<alt-2;e++){
+            System.out.print("*");
+        }
+        System.out.println(" ");
     }
 }
