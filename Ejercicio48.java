ac913dcffcc0aa214e4597b4149c4e65243b4b9b
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-29 18:06:50 +0200

    Ejercicio48

---
+++ Ejercicio48.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Ejercicio48{
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa te dice los números que aparecen y los que no");
+        System.out.print("Dime un número: ");
+        int n = sc.nextInt();
+        int nv = 0;
+        int nc = 0;
+        int t = 0;
+        int t1 = 0;
+        boolean igual;
+        System.out.print("Dígitos que aparecen en el número: ");
+        for(int i = 0;i<10;i++){
+            igual = false;
+            nc = n;
+            while(nc>0){
+                if((nc % 10)==i){
+                    igual = true;
+                }
+                nc/=10;
+            }
+            if(igual){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println("");
+        System.out.print("Dígitos que aparecen en el número: ");
+        for(int i = 0;i<10;i++){
+            igual = false;
+            nc = n;
+            while(nc>0){
+                if((nc % 10)==i){
+                    igual = true;
+                }
+                nc/=10;
+            }
+            if(!igual){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
