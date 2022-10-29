a16c5b049607cad71fec72c90ccf7097eff7b6f6
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-29 11:57:02 +0200

    Ejercicio44

---
+++ Ejercicio44.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ejercicio44{
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa introduce un número dentro de otro");
+        System.out.print("Dime un número: ");
+        long n = sc.nextLong();
+        long nc = n;
+        System.out.print("Dime una posición: ");
+        int pos = sc.nextInt();
+        System.out.print("Dime el número a introducir: ");
+        int d = sc.nextInt();
+        int cont = 0;
+        while(n>0){
+            cont++;
+            n/=10;
+        }
+        long pizq = nc / (long)Math.pow(10, cont-pos+1);
+        pizq = pizq*10+d;
+        long pder = nc % (long)Math.pow(10, cont-pos+1);
+        n = pizq* (long)(Math.pow(10, cont-pos+1))+pder;
+        System.out.println("El número resultante es: "+n);
+    }
+}
